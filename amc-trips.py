7585c380eca1c81f7228f795870f3a4dbc5dce7c
Brant Cheikes <brant@pedal-dancer.net>
2023-03-06 17:28:59 -0500

    Incomplete set of changes

--- amc-trips.py
+++ amc-trips.py
@@ -12,30 +12,8 @@
 # - For each leader, counts the number of trips as leader, coleader, and cancelled.
 
 import csv
+import sys
 import datetime as dt
-
-# program constants
-# This program expects to find two input files in the 'tripdata' subfolder.
-# modify INPUTFILE to contain the base name of the CSV file to be analyzed, leaving off the ".csv" part
-TRIPDATA_LOC = 'tripdata'
-PATH_SEP = '/'
-TRIPFILE = TRIPDATA_LOC+PATH_SEP+'trips'
-LEADERFILE = TRIPDATA_LOC+PATH_SEP+'leaders'
-LEADER_FIELDS = ['TripLeader1','TripLeader2','TripLeader3','TripLeader4']
-COLEADER_FIELDS = ['TripCoLeader1','TripCoLeader2']
-
-# Global Variables
-Active_Committees = dict()
-Leaders_By_ID = dict()
-Leaders_By_Name = dict()
-Trips = list()
-Open_Trips = 0
-Cancelled_Trips = 0
-Waitlisted_Trips = 0
-Full_Trips = 0
-Earliest_Trip = dt.datetime(dt.MAXYEAR,12,31)
-Latest_Trip = dt.datetime(dt.MINYEAR,1,1)
-Next_Fake_ID = -1
 
 def normalized_name(name_string):
     # convert name_string to lower case and replace whitespaces with underscores
@@ -67,6 +45,7 @@ class Leader:
         self.known = True
         self.email = email
         self.names = list()
+        self.committees = list()
         if len(email) > 0:
             self.email = email.lower()
         self.trip_dates = set()
@@ -104,6 +83,11 @@ class Leader:
         if fullname not in self.names:
             self.names += [fullname]
         return fullname
+    
+    def add_committee(self,committee_name):
+        if not(committee_name in self.committees):
+            self.committees += [committee_name]
+        return self.committees
 
     def add_leader_credit(self,tripdate):
         if tripdate in self.trip_dates:
@@ -169,145 +153,190 @@ def dictify(field_names, field_values):
         new_dict[this_field] = this_value
     return new_dict
 
-# main program
-# Part 1: Load the file of known activity leaders.
-# We keep track of all the leaders we've seen, using their unique ID as supplied in the ActDB data
-# Note that we have to be watchful for duplicate IDs, which could actually contain new names!
-with open(LEADERFILE+'.csv','r') as csvfile:
-    print('loading leader list from ',LEADERFILE)
-    csvreader = csv.reader(csvfile)
-    # load the first row which contains a comma-separated list of field names
-    fields = csvreader.__next__()
-    # iterate over the remaining rows
-    for row in csvreader:
-        row_dict = dictify(fields,row)
-        # each row contains a ConstituentID which is unique for each unique person
-        this_id = row_dict['ConstituentID']
-        if this_id in Leaders_By_ID:
-            # we have already processed a row with this ID, so grab the existing Leader instance
-            leader = Leaders_By_ID[this_id]
-        else:
-            # first time we've seen this ID so create a new Leader instance
-            this_email = row_dict['Email']
-            leader = Leader(this_id,this_email)
-            Leaders_By_ID[this_id] = leader
-        # add names from this record. Two versions are supplied in the record.
-        leader.add_name(row_dict['FirstName'],row_dict['LastName'],row_dict['MiddleInitial'])
-        leader.add_name(row_dict['REFirstName'],row_dict['RELastName'],row_dict['REMiddleInitial'])
-        # associate each name variant found in the data with the same leader record
-        for name in leader.names:
-            Leaders_By_Name[name] = leader
+# need to eliminate all the globals!!
+# program constants
+LEADER_FIELDS = ['TripLeader1','TripLeader2','TripLeader3','TripLeader4']
+COLEADER_FIELDS = ['TripCoLeader1','TripCoLeader2']
 
-# at this point, the Leaders_By_ID dictionary should contain a list of unique constitutent IDs associated with a unique Leader instance.
-print('loaded data on',len(Leaders_By_ID),'leaders')
+# Global Variables
+Active_Committees = dict()
+Trips = list()
+Open_Trips = 0
+Cancelled_Trips = 0
+Waitlisted_Trips = 0
+Full_Trips = 0
+Earliest_Trip = dt.datetime(dt.MAXYEAR,12,31)
+Latest_Trip = dt.datetime(dt.MINYEAR,1,1)
+Next_Fake_ID = -1
 
-# Part 2: Load all the trip listing data.
-with open(TRIPFILE+'.csv','r') as csvfile:
-    print('analyzing trip listings in',TRIPFILE)
-    csvreader = csv.reader(csvfile)
-    fields = csvreader.__next__()
-    for row in csvreader:
-        # parse the row into a dictionary matching field names to field values
-        this_trip = dictify(fields,row)
-        # save this dictionary in our list of trips
-        Trips += [this_trip]
-        # Inspect the committee, and update the dictionary mapping committees to trip counts
-        this_committee = this_trip['Committee']
-        if this_committee in Active_Committees:
-            Active_Committees[this_committee] += 1
-        else:
-            Active_Committees[this_committee] = 1
-        this_date = dt.datetime.strptime(this_trip['TripStartDate'],'%m/%d/%Y')
-        # update values of Earliest_Trip and Latest_Trip if appropriate
-        if this_date < Earliest_Trip:
-            Earliest_Trip = this_date
-        if this_date > Latest_Trip:
-            Latest_Trip = this_date
-        this_status = this_trip['TripStatus']
-        # update the count of trips with each of four different status values
-        if this_status == 'O':
-            Open_Trips += 1
-        elif this_status == 'C':
-            Cancelled_Trips += 1
-        elif this_status == 'W':
-            Waitlisted_Trips += 1
-        elif this_status == 'F':
-            Full_Trips += 1
+# load leaders from the leaderfile
+def load_leaders(leaderfile):
+    # Load and process the file of known activity leaders.
+    # We keep track of all the leaders we've seen, using their unique ID as supplied in the ActDB data
+    # Note that we have to be watchful for duplicate IDs, which could actually contain new names!
+    leaders_by_id = dict()
+    leaders_by_name = dict()
+    with open(leaderfile,'r') as csvfile:
+        print('Loading leaders from ',leaderfile)
+        csvreader = csv.reader(csvfile)
+        # load the first row which contains a comma-separated list of field names
+        fields = csvreader.__next__()
+        # iterate over the remaining rows
+        for row in csvreader:
+            # 'dictify' this row of data, associating each field in the header with a value
+            row_dict = dictify(fields,row)
+            # each row will contain a ConstituentID that is unique for each unique person
+            this_id = row_dict['ConstituentID']
+            print("processing ConstituentID", this_id)
+            if this_id in leaders_by_id:
+                # we have already processed a row with this ID, so grab the existing Leader instance
+                leader = leaders_by_id[this_id]
+            else:
+                # first time we've seen this ID so create a new Leader instance
+                this_email = row_dict['Email']
+                leader = Leader(this_id,this_email)
+                leaders_by_id[this_id] = leader
+            # add names from this record. Two versions are supplied in the record.
+            leader.add_name(row_dict['FirstName'],row_dict['LastName'],row_dict['MiddleInitial'])
+            leader.add_name(row_dict['REFirstName'],row_dict['RELastName'],row_dict['REMiddleInitial'])
+            # associate each name variant found in the data with the same leader record
+            for name in leader.names:
+                leaders_by_name[name] = leader
+            # add the committee affiliation
+            leader.add_committee(row_dict['Committee'])
 
-print('statistics on',len(Trips),'trips:')
-print('trip data spans',Earliest_Trip.strftime("%m/%d/%Y"),'to',Latest_Trip.strftime("%m/%d/%Y"))
-print(Open_Trips,'Open,',Full_Trips,'Full,',Waitlisted_Trips,'Waitlisted,',Cancelled_Trips,'cancelled')
+    # at this point, the Leaders_By_ID dictionary should contain a list of unique constitutent IDs associated with a unique Leader instance.
+    print('loaded data on',len(leaders_by_id),'leaders')
+    return [leaders_by_id, leaders_by_name]
 
-# now let's iterate over all the trips and separately process leaders and coleaders
-for trip in Trips:
-    status = trip['TripStatus']
-    trip_date = trip['TripStartDate']
-    # extract the lists of leaders and coleaders. These typically are name strings like 'Jane Q Public'.
-    leaders = [trip[leader] for leader in LEADER_FIELDS]
-    coleaders = [trip[coleader] for coleader in COLEADER_FIELDS]
-    # iterate over the leader names
-    for leader_name in leaders:
-        if len(leader_name) > 0:
-            # non-blank leader name
-            normalized = normalized_name(leader_name)
-            if normalized not in Leaders_By_Name:
-                # this leader name was not previously found in the leader data
-                # create a new Leader instance, mark it as not known, set the name, add to Leaders_By_Name
-                leader = Leader(Next_Fake_ID,'')
-                parts = normalized.split('_',2)
-                if len(parts) == 2:
-                    leader.add_name(parts[0],parts[1],'')
+def load_trips(tripfile):
+    # Load all the trip listing data.
+    with open(tripfile,'r') as csvfile:
+        print('analyzing trip listings in',tripfile)
+        csvreader = csv.reader(csvfile)
+        fields = csvreader.__next__()
+        for row in csvreader:
+            # parse the row into a dictionary matching field names to field values
+            this_trip = dictify(fields,row)
+            # save this dictionary in our list of trips
+            Trips += [this_trip]
+            # Inspect the committee, and update the dictionary mapping committees to trip counts
+            this_committee = this_trip['Committee']
+            if this_committee in Active_Committees:
+                Active_Committees[this_committee] += 1
+            else:
+                Active_Committees[this_committee] = 1
+            this_date = dt.datetime.strptime(this_trip['TripStartDate'],'%m/%d/%Y')
+            # update values of Earliest_Trip and Latest_Trip if appropriate
+            if this_date < Earliest_Trip:
+                Earliest_Trip = this_date
+            if this_date > Latest_Trip:
+                Latest_Trip = this_date
+            this_status = this_trip['TripStatus']
+            # update the count of trips with each of four different status values
+            if this_status == 'O':
+                Open_Trips += 1
+            elif this_status == 'C':
+                Cancelled_Trips += 1
+            elif this_status == 'W':
+                Waitlisted_Trips += 1
+            elif this_status == 'F':
+                Full_Trips += 1
+
+    print('statistics on',len(Trips),'trips:')
+    print('trip data spans',Earliest_Trip.strftime("%m/%d/%Y"),'to',Latest_Trip.strftime("%m/%d/%Y"))
+    print(Open_Trips,'Open,',Full_Trips,'Full,',Waitlisted_Trips,'Waitlisted,',Cancelled_Trips,'cancelled')
+
+def analyze_trips(trips, leaders_by_name, leaders_by_id):
+    # now let's iterate over all the trips and separately process leaders and coleaders
+    for trip in Trips:
+        status = trip['TripStatus']
+        trip_date = trip['TripStartDate']
+        # extract the lists of leaders and coleaders. These typically are name strings like 'Jane Q Public'.
+        leaders = [trip[leader] for leader in LEADER_FIELDS]
+        coleaders = [trip[coleader] for coleader in COLEADER_FIELDS]
+        # iterate over the leader names
+        for leader_name in leaders:
+            if len(leader_name) > 0:
+                # non-blank leader name
+                normalized = normalized_name(leader_name)
+                if normalized not in leaders_by_name:
+                    # this leader name was not previously found in the leader data
+                    # create a new Leader instance, mark it as not known, set the name, add to Leaders_By_Name
+                    leader = Leader(Next_Fake_ID,'')
+                    parts = normalized.split('_',2)
+                    if len(parts) == 2:
+                        leader.add_name(parts[0],parts[1],'')
+                    else:
+                        leader.add_name(parts[0],parts[2],parts[1])
+                    leader.known = False
+                    leaders_by_name[normalized] = leader
+                    leaders_by_id[Next_Fake_ID] = leader
+                    Next_Fake_ID -= 1
                 else:
-                    leader.add_name(parts[0],parts[2],parts[1])
-                leader.known = False
-                Leaders_By_Name[normalized] = leader
-                Leaders_By_ID[Next_Fake_ID] = leader
-                Next_Fake_ID -= 1
-            else:
-                leader = Leaders_By_Name[normalized]
-            if status == 'C':
-                leader.trips_cancelled += 1
-            else:
-                leader.add_leader_credit(trip_date)
-    for coleader_name in coleaders:
-       if len(coleader_name) > 0:
-            # non-blank leader name
-            normalized = normalized_name(coleader_name)
-            if normalized not in Leaders_By_Name:
-                # this coleader name was not previously found in the leader data
-                # these unapproved leaders are assigned negative 'fake' id numbers 
-                leader = Leader(Next_Fake_ID,'')
-                parts = normalized.split('_',2)
-                if len(parts) == 2:
-                    leader.add_name(parts[0],parts[1],'')
+                    leader = leaders_by_name[normalized]
+                if status == 'C':
+                    leader.trips_cancelled += 1
                 else:
-                    leader.add_name(parts[0],parts[2],parts[1])
-                leader.known = False
-                Leaders_By_Name[normalized] = leader
-                Leaders_By_ID[Next_Fake_ID] = leader
-                Next_Fake_ID -= 1
-            else:
-                leader = Leaders_By_Name[normalized]
-            if status == 'C':
-                leader.trips_cancelled += 1
-            else:
-                leader.add_coleader_credit(trip_date)
+                    leader.add_leader_credit(trip_date)
+        for coleader_name in coleaders:
+            if len(coleader_name) > 0:
+                    # non-blank leader name
+                    normalized = normalized_name(coleader_name)
+                    if normalized not in leaders_by_name:
+                        # this coleader name was not previously found in the leader data
+                        # these unapproved leaders are assigned negative 'fake' id numbers 
+                        leader = Leader(Next_Fake_ID,'')
+                        parts = normalized.split('_',2)
+                        if len(parts) == 2:
+                            leader.add_name(parts[0],parts[1],'')
+                        else:
+                            leader.add_name(parts[0],parts[2],parts[1])
+                        leader.known = False
+                        leaders_by_name[normalized] = leader
+                        leaders_by_id[Next_Fake_ID] = leader
+                        Next_Fake_ID -= 1
+                    else:
+                        leader = leaders_by_name[normalized]
+                    if status == 'C':
+                        leader.trips_cancelled += 1
+                    else:
+                        leader.add_coleader_credit(trip_date)
 
-# count all the active leaders in Leaders_By_ID
-print('finished processing all the trips, and analyzing leaders and coleaders')
+if __name__ == "__main__":
+    print("Preparing to analyze data from AMC Activities Database")
+    leader_file = sys.argv[1]
+    trip_file = sys.argv[2]
+   
+    # load the leaders
+    print("Step 1: loading leaders")
+    result = load_leaders(leader_file)
+    leaders_by_id = result[0]
+    leaders_by_name = result[1]
+    
+    # stopped editing here!!
+    # print("Step 2: load the trips")
+    # load_trips(tripf)
+    # print("Step 3: Analyze trips")
+    # analyze_trips()
+    # print("Done with analysis, writing output files")
 
-print('writing leader data to',TRIPFILE+'-leaderdata.csv')
-with open(TRIPFILE+'-leaderdata.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile,fieldnames=Leader.fieldnames)
-    writer.writeheader()
-    for leader in Leaders_By_ID:
-        writer.writerow(Leaders_By_ID[leader].as_dict())
+    """ output_file_1 = trip_file_base+'-leaderdata.csv'
+    print('Writing leader data to',output_file_1)
+    with open(output_file_1, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile,fieldnames=Leader.fieldnames)
+        writer.writeheader()
+        for leader in Leaders_By_ID:
+            writer.writerow(Leaders_By_ID[leader].as_dict())
 
-print('writing committee data to',TRIPFILE+'-committeedata.csv')
-with open(TRIPFILE+'-committeedata.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for c in Active_Committees:
-        writer.writerow([c,Active_Committees[c]])
+    output_file_2 = trip_file_base+'-committeedata.csv'
+    print('Writing committee data to',output_file_2)
+    with open(output_file_2, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for c in Active_Committees:
+            writer.writerow([c,Active_Committees[c]]) """
 
-print('done!')
-# write it all out to CSV files - statistics, per committee, per leader
+    print('Done!')
+    
+# run main() if we are called from the command line
+
